58f11210caeb5892702890f6582ffb7dac847bb3
Elise Hamm <elise.rose.hamm@gmail.com>
2024-01-25 16:08:18 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4748;
 const About = require('./serveur.json');
 
-app.use(express.json());
+app.use(cors());
 
 app.get("/api/user",(req, res) => 
     {res.json(About)}
